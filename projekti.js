46c73546388ffc39b519cf40d6fc1c6b9f0effa6
NooaMannisto <88773537+NooaMannisto@users.noreply.github.com>
2021-08-19 07:31:05 +0000

    2021/08/18 ööööööööööööö

--- projekti.js
+++
@@ -1,6 +0,0 @@
-function checkForBlank(){
-  if (document.getElementById("answer").value == "") {
-    document.getElementById("subcim_error_message").InnerHTML="You must add a title!";
-    return false;
-}
-}
---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+let pvm="2021/08/18";
+
+function pvmFin(paivamaara oikeassa jarjestyksessa on: vuosi, kk, pv){
+
+    return "Paivamaara oikeassa jarjestyksessa" on ","pv+"."+kk+"."+vuosi;
+}
+
+
+
+
+
+console.log(pvmFin(pvm.split("/")[0], pvm.split("/")[1],
+pvm.split("/")[2]));
+
+
+
+
+
+
+/*
+Monen 
+rivin 
+kommentti
+*/
